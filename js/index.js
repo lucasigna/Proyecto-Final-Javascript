11ead141175a418f0a6b36bdfca7f25dba13ba38
Lucas Ignacio González <lucasignaciogonzalez1999@gmail.com>
2021-08-30 23:57:26 -0300

    Comentando el código

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 $(window).ready(function() {
-
+    // Creo una variable para la raíz de la app y otra para el header
     let app = $('#app');
     let header = $('header');
-
+    // Almaceno las rutas
     const routes = [
             { path: '/', page: 'conversor' },
             { path: '/buscador', page: 'buscador' },
         ];
-
+    // Función que obtiene la ruta actual
     const parseLocation = () => location.hash.slice(1).toLocaleLowerCase() || '/';
-
+    // Función que obtiene la página según la ruta
     const findPageByPath = (path, routes) => routes.find(route => route.path == path);
 
     const router = () => {
